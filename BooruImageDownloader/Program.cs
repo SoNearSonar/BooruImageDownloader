627bf9f44819e3177ce308e5d7e2720cf9b8f122
SoNearSonar <sonearsonar@gmail.com>
2023-02-09 11:14:18 -0500

    Add check for > 1 program running

--- BooruImageDownloader/Program.cs
+++ BooruImageDownloader/Program.cs
@@ -8,6 +8,15 @@ namespace BooruImageDownloader
         [STAThread]
         static void Main()
         {
+            string appName = "Booru Image Downloader";
+            _ = new Mutex(true, appName, out bool appNotLoaded);
+
+            if (!appNotLoaded)
+            {
+                MessageBox.Show("You cannot have more than one instance of this program running", $"{appName} is already running", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
